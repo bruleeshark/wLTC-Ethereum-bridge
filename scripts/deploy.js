c3bef2d9da363c07a0bcf81633f7a3cd87f2974e
bruleeshark <95983960+bruleeshark@users.noreply.github.com>
2022-12-23 12:14:59 -0700

    v2 w changes recommended by chatgpt
    R: Can you help me check the following deploy script for bugs? 
    
    const { deployment } = require("hardhat/config");
    
    async function main() {
      const LitecoinToken = await ethers.getContractFactory("LitecoinToken");
      const contract = await LitecoinToken.deploy();
      await contract.deployed();
      console.log("LitecoinToken deployed to:", contract.address);
    }
    
    main()
      .then(() => process.exit(0))
      .catch((error) => {
        console.error(error);
        process.exit(1);
      });
    
    const contract = await LitecoinToken.deploy(owner);
    
    chatgpt: Here are a few suggestions for improving the deploy script:
    
    It's a good idea to specify the deployment account in the hardhat.config.js file, so that you can easily switch between accounts when deploying the contract. You can do this by adding the following line to the hardhat.config.js file:
    Copy code
    deployment.accounts = [
      {
        privateKey: "0x...",
        balance: "100000000000000000000",
      },
      // Add more accounts here if needed
    ];
    In the main() function, you can use the deploy() method to specify the contract owner when deploying the contract. For example:
    Copy code
    const contract = await LitecoinToken.deploy(owner);
    It's a good idea to handle errors in the catch block of the main() function. For example:
    Copy code
    main()
      .then(() => process.exit(0))
      .catch((error) => {
        console.error(error);
        process.exit(1);
      });
    
    I hope these suggestions are helpful! Let me know if you have any other questions.
    
    R: can you show me an example deploy script with the changes you recommended?
    
    chatgpt: Certainly! Here is an example deploy script that incorporates the suggestions I mentioned earlier:
    
    const { deployment } = require("hardhat/config");
    
    async function main() {
      // Set the deployment account in the hardhat.config.js file
      deployment.accounts = [
        {
          privateKey: "0x...",
          balance: "100000000000000000000",
        },
        // Add more accounts here if needed
      ];
    
      const LitecoinToken = await ethers.getContractFactory("LitecoinToken");
    
      // Specify the contract owner when deploying the contract
      const contract = await LitecoinToken.deploy(owner);
      await contract.deployed();
      console.log("LitecoinToken deployed to:", contract.address);
    }
    
    main()
      .then(() => process.exit(0))
      .catch((error) => {
        console.error(error);
        process.exit(1);
      });
    
    I hope this helps! Let me know if you have any other questions.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,19 @@
 const { deployment } = require("hardhat/config");
 
 async function main() {
+  // Set the deployment account in the hardhat.config.js file
+  deployment.accounts = [
+    {
+      privateKey: "0x...",
+      balance: "100000000000000000000",
+    },
+    // Add more accounts here if needed
+  ];
+
   const LitecoinToken = await ethers.getContractFactory("LitecoinToken");
-  const contract = await LitecoinToken.deploy();
+
+  // Specify the contract owner when deploying the contract
+  const contract = await LitecoinToken.deploy(owner);
   await contract.deployed();
   console.log("LitecoinToken deployed to:", contract.address);
 }
@@ -13,5 +24,3 @@ main()
     console.error(error);
     process.exit(1);
   });
-
-const contract = await LitecoinToken.deploy(owner);
